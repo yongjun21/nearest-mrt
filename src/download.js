a8f0ffcb6b35513972fafa56756a2d8d8f9744ba
Yong Jun <yjthong.2009@smu.edu.sg>
2016-12-13 11:23:17 +0800

    Added LRT stations data

--- src/download.js
+++ src/download.js
@@ -2,17 +2,25 @@ import fs from 'fs'
 import range from 'lodash/range'
 import fetch from 'node-fetch'
 
-const url = 'https://developers.onemap.sg/commonapi/search?searchVal=mrt station&returnGeom=Y&getAddrDetails=Y&pageNum='
+// Promise.all(generateApiCalls('mrt station', 80)).then(results => {
+//   const result = results.reduce((a, v) => a.concat(v), [])
+//     .filter(v => v['SEARCHVAL'].match(/MRT STATION, /))
+//   console.log(result.length)
+//   fs.writeFileSync('data/raw/mrt.json', JSON.stringify(result, null, '\t'))
+// })
 
-const apiCalls = range(80).map(i => {
-  return fetch(url + (i + 1))
-    .then(res => res.json())
-    .then(json => json.results)
-})
-
-Promise.all(apiCalls).then(results => {
+Promise.all(generateApiCalls('lrt station', 50)).then(results => {
   const result = results.reduce((a, v) => a.concat(v), [])
-    .filter(v => v['SEARCHVAL'].match(/MRT STATION, /))
   console.log(result.length)
-  fs.writeFileSync('data/raw.json', JSON.stringify(result, null, '\t'))
+  fs.writeFileSync('data/raw/lrt.json', JSON.stringify(result, null, '\t'))
 })
+
+function generateApiCalls (searchVal, pages) {
+  return range(pages).map(i => {
+    const url = 'https://developers.onemap.sg/commonapi/search?searchVal=' + searchVal +
+      '&returnGeom=Y&getAddrDetails=Y&pageNum=' + (i + 1)
+    return fetch(url)
+      .then(res => res.json())
+      .then(json => json.results)
+  })
+}
